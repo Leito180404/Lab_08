04134c1737befacb6b0ea42988a3327683a97715
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-21 22:12:35 -0500

    Create BinarySearchTree.java

---
+++ Actividades/avltree/BinarySearchTree.java
@@ -0,0 +1,9 @@
+package avltree;
+import Excepciones.*;
+
+public interface BinarySearchTree<E> {   
+    void insert(E data) throws ItemDuplicated;
+    E search(E data) throws ItemNoFound;
+    void delete(E data) throws ExceptionEmpty;
+    boolean isEmpty();
+}

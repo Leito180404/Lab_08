7f9f49014f74b726127fe1c69adf8b9252755047
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-21 21:47:32 -0500

    Update ExceptionEmpty.java

--- Actividades/Excepciones/ExceptionEmpty.java
+++ Actividades/Excepciones/ExceptionEmpty.java
@@ -1,5 +1,12 @@
 package Excepciones;
 
-public class ExceptionEmpty {
-    
+public class ExceptionEmpty extends Exception {
+    public ExceptionEmpty(String msg) {
+        super(msg);
+    }
+
+    public ExceptionEmpty() {
+        super("Esta vacio.");
+    }
 }
+

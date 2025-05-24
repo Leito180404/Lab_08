510ec1434b0ba7fb346cb47ccd17349686c3701c
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-24 12:21:58 -0500

    Update AVLTree2.java

--- Ejercicios/AVLTree2.java
+++ Ejercicios/AVLTree2.java
@@ -220,5 +220,5 @@ public class AVLTree2<E extends Comparable<E>> extends BSTree<E> {
         System.out.print(node.data + " ");
         preordenRec(node.left);
         preordenRec(node.right);
-    }
+    }   
 }

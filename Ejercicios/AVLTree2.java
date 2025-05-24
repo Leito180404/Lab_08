a8e2c7042ecf0a779621cfe048c09bf3b144dc67
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-24 11:28:57 -0500

    Create AVLTree2.java

---
+++ Ejercicios/AVLTree2.java
@@ -0,0 +1,167 @@
+import Excepciones.ItemDuplicated;
+import Excepciones.ItemNoFound;
+import avltree.*;
+
+public class AVLTree2<E extends Comparable<E>> extends BSTree<E> {
+    class NodeAVL extends Node {
+        protected int bf;  // factor de equilibrio
+
+        public NodeAVL(E data) {
+            super(data);
+            this.bf = 0;  // Inicializamos el factor de equilibrio en 0
+        }
+
+        @Override
+        public String toString() {
+            return data.toString() + "(" + bf + ")";
+        }
+    }
+
+    private boolean height;  // indicador de cambio de altura
+
+    @Override
+    public void insert(E x) throws ItemDuplicated {
+        this.height = false;
+        this.root = insert(x, (NodeAVL) this.root);
+    }
+
+    protected Node insert(E x, NodeAVL node) throws ItemDuplicated {
+        NodeAVL fat = node;
+            if (node == null) {
+                this.height = true;
+                fat = new NodeAVL(x);
+            } else {
+                int resC = node.data.compareTo(x);
+            if (resC == 0)
+                throw new ItemDuplicated(x + " ya se encuentra en el arbol...");
+        if (resC < 0) {
+            fat.right = insert(x, (NodeAVL) node.right);
+            if (this.height) {
+                switch (fat.bf) {
+                    case -1:
+                        fat.bf = 0;
+                        this.height = false;
+                        break;
+                    case 0:
+                        fat.bf = 1;
+                        this.height = true;
+                        break;
+                    case 1:
+                        fat = balanceToLeft(fat);
+                        this.height = false;
+                        break;
+                }
+            }
+        } else {
+            fat.left = insert(x, (NodeAVL) node.left);
+            if (this.height) {
+                switch (fat.bf) {
+                    case 1:
+                        fat.bf = 0;
+                        this.height = false;
+                        break;
+                    case 0:
+                        fat.bf = -1;
+                        this.height = true;
+                        break;
+                    case -1:
+                        fat = balanceToRight(fat);
+                        this.height = false;
+                        break;
+                }
+            }
+        }
+    }
+    return fat;
+    }
+
+    private NodeAVL balanceToLeft(NodeAVL node){
+    NodeAVL hijo = (NodeAVL)node.right;
+    switch(hijo.bf) {
+        case 1:
+            node.bf = 0;
+            hijo.bf = 0;
+            node = rotateSL(node);
+            break;
+        case -1:
+            NodeAVL nieto = (NodeAVL)hijo.left;
+            switch(nieto.bf) {
+                case -1: node.bf = 0; hijo.bf = 1; break;
+                case 0:  node.bf = 0; hijo.bf = 0; break;
+                case 1:  node.bf = 1; hijo.bf = 0; break;
+            }
+            nieto.bf = 0;
+
+            node.right = rotateSR(hijo); 
+            node = rotateSL(node);
+    }
+        return node;
+    }
+    private NodeAVL balanceToRight(NodeAVL node){
+        NodeAVL hijo = (NodeAVL) node.left;
+        switch(hijo.bf) {
+            case -1:
+                node.bf = 0;
+                hijo.bf = 0;
+                node = rotateSR(node);
+                break;
+            case 1:
+                NodeAVL nieto = (NodeAVL) hijo.right;
+                switch(nieto.bf) {
+                    case -1: node.bf = 1; hijo.bf = 0; break;
+                    case 0:  node.bf = 0; hijo.bf = 0; break;
+                    case 1:  node.bf = 0; hijo.bf = -1; break;
+                }
+                nieto.bf = 0;
+                node.left = rotateSL(hijo);
+                node = rotateSR(node);
+                break;
+        }
+        return node;
+    }
+
+    private NodeAVL rotateSL(NodeAVL node){
+        NodeAVL p = (NodeAVL)node.right;
+        node.right = p.left;
+        p.left = node;
+        node = p;
+        return node;
+    }
+
+    private NodeAVL rotateSR(NodeAVL node){
+        NodeAVL p = (NodeAVL)node.left;
+        node.left = p.right;
+        p.right = node;
+        node = p;
+        return node;
+    }
+
+@Override
+public void delete(E x) throws ItemNoFound {
+    this.height = false;
+    this.root = delete(x, (NodeAVL)this.root);
+}
+
+protected NodeAVL delete(E x, NodeAVL node) throws ItemNoFound {
+    if (node == null) {
+        throw new ItemNoFound(x + " no encontrado");
+    }
+
+    int cmp = x.compareTo(node.data);
+    if (cmp < 0) {
+        node.left = delete(x, (NodeAVL) node.left);
+        if (height) {
+            node = balanceToLeft(node);
+        }
+    } else if (cmp > 0) {
+        node.right = delete(x, (NodeAVL) node.right);
+        if (height) {
+            node = balanceToRight(node);
+        }
+    } else {
+
+    }
+
+    return node;
+    }
+}
